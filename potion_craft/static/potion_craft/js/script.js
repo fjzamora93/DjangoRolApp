7318abcede816a9517f692428dd75db952e5cfec
javier <fjzamora93@gmail.com>
2024-04-04 20:37:38 -0300

    update javascript creacion personaje

--- potion_craft/static/potion_craft/js/script.js
+++ potion_craft/static/potion_craft/js/script.js
@@ -1,37 +1,77 @@
 
-
 document.addEventListener('DOMContentLoaded', function(){
     const clase_dd = document.querySelector('#id_clase');
     const raza_dd = document.querySelector('#id_raza');
     const genero_dd = document.querySelector('#id_genero');
     const avatar_dd = document.querySelector('#id_portrait');
     const portrait = document.querySelector('#retrato');
+    const portraitLeft = document.querySelector('#retrato-izquierda');
+    const portraitRight = document.querySelector('#retrato-derecha');
+
+    const botonAnterior = document.querySelector('.galeria button[value="-"]');
+    const botonSiguiente = document.querySelector('.galeria button[value="+"]');
+
 
     function construirRutaImagen(clase, raza, genero, avatar) {
-        let ruta = `/static/potion_craft/img/portraits/${raza}/${genero}-${raza}-${clase}-${avatar}.png`
-        console.log(ruta)
-        return ruta;
+        let ruta_principal = `/static/potion_craft/img/portraits/${raza}/${genero}-${raza}-${clase}-${avatar}.png`
+        var anterior = parseInt(avatar) - 1;
+        var avatarAnterior = anterior.toString();
+        var siguiente = parseInt(avatar) + 1;
+        var avatarSiguiente = siguiente.toString();
+        let ruta_anterior = `/static/potion_craft/img/portraits/${raza}/${genero}-${raza}-${clase}-${avatarAnterior}.png`
+        let ruta_siguiente = `/static/potion_craft/img/portraits/${raza}/${genero}-${raza}-${clase}-${avatarSiguiente}.png`
+        var rutas = [ruta_principal, ruta_anterior, ruta_siguiente]
+        console.log(rutas)
+        return rutas;
     }
 
-    function actualizarImagen() {
+    function actualizarImagen(boton = null) {
+        var operador = boton.value;
         let claseSeleccionada = clase_dd.value;
         let razaSeleccionada = raza_dd.value;
         let generoSeleccionado = genero_dd.value;
         let avatarSeleccionado = avatar_dd.value;
 
-        let rutaImagen = construirRutaImagen(claseSeleccionada, razaSeleccionada, generoSeleccionado, avatarSeleccionado);
-        portrait.src = rutaImagen;
-        portrait.alt = claseSeleccionada;
+        if (operador != null){
+            if (operador === "+"){
+                var intAvatar = parseInt(avatarSeleccionado) + 1;
+            } else {
+                var intAvatar = parseInt(avatarSeleccionado) - 1;
+            }
+            avatarSeleccionado = intAvatar.toString();
+            avatar_dd.value = avatarSeleccionado
+            console.log(avatarSeleccionado, avatar_dd.value)
+            
+        }
+        
+        let rutas = construirRutaImagen(claseSeleccionada, razaSeleccionada, generoSeleccionado, avatarSeleccionado);
+        portrait.src = rutas[0];
+        portraitLeft.src = rutas[1];
+        portraitRight.src = rutas[2]
     }
 
-    portrait.src = construirRutaImagen(clase_dd.value, raza_dd.value, genero_dd.value, avatar_dd.value);
-    portrait.alt = clase_dd.value;
+    //portrait.src = construirRutaImagen(clase_dd.value, raza_dd.value, genero_dd.value, avatar_dd.value);
+
 
     const selectsCharCreator = document.querySelectorAll('#charcreator select');
     selectsCharCreator.forEach(select => {
         select.addEventListener('change', actualizarImagen);
+
+        
     });
+
+    // Asignar event listeners a los botones
+    botonAnterior.addEventListener('click', function() {
+        actualizarImagen(this);
+    });
+
+    botonSiguiente.addEventListener('click', function() {
+        actualizarImagen(this);
+    });
+
 });
+
+
 
 
 
